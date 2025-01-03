95a4d554d246e42b04f4e25013cff9ac7fbfdb3d
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-01-03 13:55:24 +0530

    Navbar UI completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
@@ -6,16 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>SignIn</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle/>
-      <Button variant={"secondary"}>Click Me</Button>
+     <h1>Home page content</h1>
     </div>
   );
 }
