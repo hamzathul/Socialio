94844832f43e32293c6c8936c3b02e2e9ed451d3
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-01-11 11:36:05 +0530

    toggleLike func

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+import { getPosts, toggleLike } from "@/actions/post.action";
+import { useUser } from "@clerk/nextjs";
+import React, { useState } from "react";
 
-const PostCard = () => {
-  return (
-    <div>PostCard</div>
-  )
-}
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
 
-export default PostCard
+const PostCard = ({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) => {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(
+    post.likes.some((like) => like.userId === dbUserId)
+  );
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+  const handleAddComment = async () => {};
+
+  const handleDeletePost = async () => {};
+
+  return <div>PostCard</div>;
+};
+
+export default PostCard;
