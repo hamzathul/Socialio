bcd442c0d8750f79dedf26b9e0262b21f9fcf456
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-01-17 15:41:35 +0530

    feat: profile actions

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProfilePage = ({params}:{params:{username:string}}) => {
+    // console.log(params)
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
