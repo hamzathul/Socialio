c15e80060d4e9eafb97de1ded7beaf4cd0571419
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-01-21 11:27:39 +0530

    uploadthing configuration

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -22,7 +22,7 @@ const CreatePost = () => {
     setIsPosting(true);
     try {
       const result = await createPost(content, imageUrl);
-      if (result.success) {
+      if (result?.success) {
         // reset the form
         setContent("");
         setImageUrl("");
